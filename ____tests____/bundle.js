4507c9b19bc8a5e773b5066a0cc66ca022a0555b
Harvey Zack <laozhaochaguan@hotmail.com>
2018-01-08 19:41:20 +0800

    fix test

--- ____tests____/bundle.js
+++ ____tests____/bundle.js
@@ -9,4 +9,14 @@
 
 */
 
+var foo = 'foo';/*
+ @Author: Harvey Zack 
+
+ @Email: 717995589@qq.com 
+
+ @Homepage: http://www.zhw-island.com 
+
+ @Date: Mon Jan 08 2018 19:40:38 GMT+0800 (HKT) 
+
+*/
 var foo = 'foo';
