2ee1e2826a462aadc99fed6970ba147f1965149f
Harvey Zack <laozhaochaguan@gmail.com>
2018-01-09 09:40:33 +0800

    add coveralls

--- ____tests____/bundle.js
+++ ____tests____/bundle.js
@@ -5,18 +5,7 @@
 
  @Homepage: http://www.zhw-island.com 
 
- @Date: Mon Jan 08 2018 17:21:43 GMT+0800 (中国标准时间) 
-
-*/
-
-var foo = 'foo';/*
- @Author: Harvey Zack 
-
- @Email: 717995589@qq.com 
-
- @Homepage: http://www.zhw-island.com 
-
- @Date: Mon Jan 08 2018 19:40:38 GMT+0800 (HKT) 
+ @Date: Tue Jan 09 2018 08:47:24 GMT+0800 (中国标准时间) 
 
 */
 var foo = 'foo';
